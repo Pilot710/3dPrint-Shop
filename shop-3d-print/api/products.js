101670130e49fa9d95394242b1c2c892d8430af7
Pilot710 <michelangelopilati@outlook.it>
2026-08-21 00:16:02 +0200

    Delete shop-3d-print directory

--- shop-3d-print/api/products.js
+++
@@ -1,6 +0,0 @@
-const products = require("../data/products.json");
-
-module.exports = (req, res) => {
-  const disponibili = products.filter((p) => p.disponibile);
-  return res.status(200).json(disponibili);
-};
